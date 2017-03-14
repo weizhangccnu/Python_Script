a2233ad21d91b566282f77c7989468162aabe18d
wz <wz@localhost.localdomain>
2017-03-14 17:21:31 +0800

    visa module work correctly

--- DG4162.py
+++ DG4162.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
-import os
 import sys
 import visa
-import time
+import usbtmc
 #-----------------------------------------------------------#
 def main():
     print "Ok!"
-    rm = visa.ResourceManager()
     #instr = usbtmc.Instrument(0x1ab1, 0x0641)
     #instr.timeout = 10
     #print (instr.ask("*IDN?"))
+    rm = visa.ResourceManager()
+    print(rm.list_resources())
+    #inst = rm.open_resource('SRL/dev/ttyS0::INSTR')
+    #inst = rm.open_resource('USB0::0x1AB1::0x0641::DG4C143100554::INSTR')
+    #inst = rm.open_resource('USB0::0x1AB1::0x0641::DG4E172601737::INSTR')
+    #inst = rm.open_resource('USB0::6833::2500::DM3R170500071::0::INSTR')
+    #inst = rm.open_resource('USB0::2931::6066::MY54450197::INSTR')
+    #print (instr.query("*IDN?"))
     #instr.write("FREQ 100")
 
 #-----------------------------------------------------------#
