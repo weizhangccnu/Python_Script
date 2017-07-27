6b554e9e93be896765284a9d687566619a92f31f
weizhangccnu <296448435@qq.com>
2017-07-27 20:05:02 +0800

    DAC8568 can control seperate

--- hpdaq_socket.py
+++ hpdaq_socket.py
@@ -15,19 +15,19 @@ port = 1024                         #server port number
 ## Command interpret
 class command_interpret:
     ## constructor
-    # @param ss socket name
+    # @param[in] ss socket name
     def __init__(self, ss):
         self.ss = ss
 
     ## write config_reg
-    # @param Addr Address of the configuration register 0-31 
-    # @param Data write into the configuration register 0-65535, [15:0]
+    # @param[in] Addr Address of the configuration register 0-31 
+    # @param[in] Data write into the configuration register 0-65535, [15:0]
     def write_config_reg(self, Addr, Data):
         data = 0x00200000 + (Addr << 16) + Data
         self.ss.sendall(struct.pack('I',data)[::-1])
     
     ## read config_reg
-    # @param Addr Address of the configuration register 0-31 
+    # @param[in] Addr Address of the configuration register 0-31 
     # return 32bit data
     def read_config_reg(self, Addr):
         data = 0x80200000 + (Addr << 16) 
@@ -35,21 +35,21 @@ class command_interpret:
         return struct.unpack('I', self.ss.recv(4)[::-1])[0]
 
     ## write pulse_reg
-    # @param Data write into the pulse register 0-65535
+    # @param[in] Data write into the pulse register 0-65535
     def write_pulse_reg(self, Data):
         data = 0x000b0000 + Data
         self.ss.sendall(struct.pack('I',data)[::-1])
 
     ## read status_reg
-    # @param Addr Address of the configuration register 0-10
+    # @param[in] Addr Address of the configuration register 0-10
     def read_status_reg(self, Addr):
         data = 0x80000000 + (Addr << 16)
         self.ss.sendall(struct.pack('I',data)[::-1])
         return struct.unpack('I', self.ss.recv(4)[::-1])[0]
 
     ## write memeoy
-    # @param Addr write address of memeoy 0-65535
-    # @param Data write into memory data 0-65535
+    # @param[in] Addr write address of memeoy 0-65535
+    # @param[in] Data write into memory data 0-65535
     def write_memory(self, Addr, Data):
         data = 0x00110000 + (0x0000ffff & Addr)             #memory address LSB register
         self.ss.sendall(struct.pack('I',data)[::-1])
@@ -61,8 +61,8 @@ class command_interpret:
         self.ss.sendall(struct.pack('I',data)[::-1])
 
     ## read memory
-    # @param Cnt read data counts 0-65535
-    # @param Addr start address of read memory 0-65535
+    # @param[in] Cnt read data counts 0-65535
+    # @param[in] Addr start address of read memory 0-65535
     def read_memory(self, Cnt, Addr): 
         data = 0x00100000 + Cnt                             #write sMemioCnt
         self.ss.sendall(struct.pack('I',data)[::-1])
@@ -76,7 +76,7 @@ class command_interpret:
             print hex(struct.unpack('I', self.ss.recv(4)[::-1])[0])
 
     ## read_data_fifo
-    # @param Cnt read data counts 0-65535
+    # @param[in] Cnt read data counts 0-65535
     def read_data_fifo(self, Cnt):
         data = 0x00190000 + Cnt                             #write sDataFifoHigh address = 25
         self.ss.sendall(struct.pack('I',data)[::-1])
@@ -131,7 +131,7 @@ def test_data_fifo():
 
 #======================================================================#
 ## write 32bit data to dac8568
-# @param dat write into dac8568's data
+# @param[in] dat write into dac8568's data
 def dac8568_write_data(dat):
     cmd_interpret.write_config_reg(0,(0xffff0000&dat)>>16)  #write high word to fifo
     cmd_interpret.write_pulse_reg(0x0002)                   #generate write pulse  
@@ -140,13 +140,14 @@ def dac8568_write_data(dat):
 
 #======================================================================#
 ## test dac8568 via hpdaq FPGA development board
-# @param ch choose the dac8568 channel 0-15
-# @param data dac8568 write in digital data 0-5V
+# @param[in] ch choose the dac8568 channel 0-15
+# @param[in] data dac8568 write in digital data 0-5V
 def test_dac8568(ch, val):
-    data = int((val*65535.0)/5.0)
-    if ch >= 0 and ch <=7:
+    data = int((val*65535.0)/5.0)                           #convert voltage value to digital code
+    if ch >= 0 and ch <= 7:
         cmd_interpret.write_config_reg(1,0x0001)            #select dac8568 one
-    elif ch >7 and ch <= 15:
+    elif ch >= 8 and ch <= 15:
+        ch = ch - 8
         cmd_interpret.write_config_reg(1,0x0000)            #select dac8568 two 
     else:
         print "you input error channel unmber"
@@ -173,33 +174,76 @@ def shift_register_wr(data, clk_div):
         print bin(cmd_interpret.read_status_reg(i))
 
 #======================================================================#
+## This function is used to config  topmetal-IIa sram and array scan modules
+# @param[in] start_addr address of first data sent to sram
+# @param[in] data_to_sram data sent to sram
+def tm_sram_config(start_addr, data_to_sram): 
+    nval = int((len(data_to_sram)+7)/8)
+    addr = start_addr & ((1<<32)-1)
+    aval = [0 for i  in xrange(nval)]
+    for i in xrange(len(data_to_sram)):
+        j = i % 8
+        k = i / 8
+        aval[k] += data_to_sram[j] << (j*4)
+    for i in xrange(len(aval)):
+        cmd_interpret.write_memory(addr+i, aval[i])
+    cmd_interpret.write_memory(len(aval), addr)
+
+#======================================================================#
+## topmetal array scan config function
+# @param[in] clk_div clock frequency division factor when array is scanning
+# @param[in] wr_clk_div clock frequency division factor when writing data into pixel
+# @param[in] stop_addr controls where scanning stop
+def tm_array_scan(clk_div, wr_clk_div, stop_addr, trig_rate, trig_delay, stop_clk_s, keep_we):
+    config_reg = ((trig_delay & 0xffff) << 48) | ((trig_rate & 0xffff) << 32) | ((stop_addr & 0xffff) <<16) | ((keep_we & 0x1) << 9) | ((stop_clk_s & 0x1) << 8) | ((wr_clk_div & 0xf) << 4) | (clk_div & 0xf)
+    for i in xrange(4):
+        cmd_interpret.write_config_reg(i+11, (config_reg >> i*16) & 0xffff) 
+    cmd_interpret.write_pulse_reg(0x0004)                   #pulse_reg[2] generate a pulse
+#======================================================================#
 ## main function
 #
 def main():
-    #test_config_reg()  
-    #test_pulse_reg()  
-    #test_status_reg()
-    #test_memory()
-    #test_data_fifo()
-    ## test dac8568 
-    #for i in xrange(10):
-    #    print i
-    #    for ch in xrange(16):
-    #        test_dac8568(ch, i*0.33)
-    #        time.sleep(0.1) 
-    #    time.sleep(5) 
-    ## test shift register 
-    data_in = 12345678987
+    ## config dac8568 
+    VBTAIL_RCF_IN = 0.74                                #dac8568 channel 0 ----> VBTAIL_RCF_IN default value 0.74V
+    VREF_RCF_IN = 0.1                                   #dac8568 channel 1 ----> VREF_RCF_IN default value 0.1V
+    VBP_RCF_IN = 2.2                                    #dac8568 channel 2 ----> VBP_RCF_IN default value 2.2V
+    SF1_IBP_RCF_IN = 1.6                                #dac8568 channel 3 ----> SF1_IBP_RCF_IN default value 1.6V
+    CSA_VBN = 0.77                                      #dac8568 channel 4 ----> CSA_VBN default value 0.77V
+    CSA_VRSTL_VG = 3.3                                  #dac8568 channel 5 ----> CSA_VRSTL_VG default value 3.3V
+    CSA_VRSTL = 0.23                                    #dac8568 channel 6 ----> CSA_VRSTL default value 0.23V
+    CSA_VRSTH = 0.3                                     #dac8568 channel 7 ----> CSA_VRSTH default value 0.3V
+    COL_IB_ICF_IN = 0.4                                 #dac8568 channel 12 ----> COL_IB_ICF_IN default value 0.4V, the rest of channel's output value are 0V
+    dac_val = [VBTAIL_RCF_IN, VREF_RCF_IN, VBP_RCF_IN, SF1_IBP_RCF_IN, CSA_VBN, CSA_VRSTL_VG, CSA_VRSTL, CSA_VRSTH, 0, 0, 0, 0, COL_IB_ICF_IN, 0, 0, 0]
+    for i in xrange(len(dac_val)):
+        print i, dac_val[i]
+        test_dac8568(i, dac_val[i])                          
+        time.sleep(0.1)
+    ## config shift register 
+    data_in = 123456789087
     clk_div = 7    
     shift_register_wr(data_in, clk_div)
+    ## config analog scan module
+    start_addr = 0
+    data_to_sram = [0x8 for i in xrange(9720)]
+    tm_sram_config(start_addr, data_to_sram)            #config sram
+    time.sleep(3)
+    
+    clk_div = 5
+    wr_clk_div = 5
+    stop_addr = 0x8013                                  #0x8013  pixel(0,19)
+    trig_rate = 4
+    trig_delay = 1
+    stop_clk_s = 0                                      #0: keep clk running 1: clk stop
+    keep_we = 1
+    tm_array_scan(clk_div, wr_clk_div, stop_addr, trig_rate, trig_delay, stop_clk_s, keep_we)
     print "OK!"
 #======================================================================#
 if __name__ == "__main__":
     ## host socket
-    # @param AF_UNIX:AF_Local, base on the local
-    # @param AF_NETLINK:linux operating system support socket
-    # @param AF_INET:base on IPV4 network TCP/UDP socket
-    # @param AF_INET6:base on IPV6 network TCP/UDP socket
+    # @param[in] AF_UNIX:AF_Local, base on the local
+    # @param[in] AF_NETLINK:linux operating system support socket
+    # @param[in] AF_INET:base on IPV4 network TCP/UDP socket
+    # @param[in] AF_INET6:base on IPV6 network TCP/UDP socket
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #establish socket
     s.connect((hostname, port))                                 #connet socket
     cmd_interpret = command_interpret(s)                        #define a class
