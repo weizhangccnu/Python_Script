55457eb784d81a3ba4149781d87ea197863f4f57
weizhangccnu <296448435@qq.com>
2017-08-13 20:34:33 +0800

    Upload Test_h5py file

---
+++ HDF5/Test_h5py.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import os 
+import sys
+import h5py
+import time
+
+#================================================================#
+def main():
+    print "OK"
+
+#================================================================#
+if __name__ == "__main__":
+    sys.exit(main())
