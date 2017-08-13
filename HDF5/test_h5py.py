fdd9da16cd665be039204127f622fe8528240ee7
root <root@localhost.localdomain>
2017-08-13 20:42:39 +0800

    Upload h5py test file

---
+++ HDF5/test_h5py.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import os 
+import sys
+import h5py
+import time
+
+#================================================================#
+def main():
+    print "OK"
+    for i in xrange(10):
+        print i
+#================================================================#
+if __name__ == "__main__":
+    sys.exit(main())
