edcd46cdbf3ee89f17a6c72056699143e4e29f88
weizhangccnu <296448435@qq.com>
2017-08-14 18:02:14 +0800

    hdf5 groups and datasets

--- HDF5/test_h5py.py
+++ HDF5/test_h5py.py
@@ -7,12 +7,22 @@ import time
 """@package h5py
 This python file is used to test h5py package
 """
-# @author WeiZhang
+## @author WeiZhang
 #================================================================#
 def main():
-    print "OK"
+    f = h5py.File('myfile.hdf5','w')
+    print f.id
+    print f.keys()
+    grp = f.create_group("bar")
+    print grp.name
+    subgrp = grp.create_group("baz")
+    print subgrp.name
+    grp["name"] = 45
+    out = grp["name"]
+    print out
     for i in xrange(10):
         print i
+    f.close()
 #================================================================#
 if __name__ == "__main__":
     sys.exit(main())
