55168eff623020b228cc837af13a68612a4fa362
weizhangccnu <296448435@qq.com>
2017-08-22 20:27:23 +0800

    fetch 100w waveform data

--- HDF5/test_h5py.py
+++ HDF5/test_h5py.py
@@ -9,56 +9,20 @@ import numpy as np
 This python file is used to test h5py package
 """
 ## @author WeiZhang
-#================================================================#
+#----------------------------------------------------------------#
 def main():
-    f = h5py.File('myfile.hdf5','w')
-    dataset = f.create_dataset("DS1", (3200,6400), 'i', chunks=(4,8), compression='gzip', compression_opts=9) 
-    data = np.zeros((3200,6400))
-    print data
-    for i in xrange(3200):
-        for j in xrange(6400):
-            data[i][j] = i*j-j
-    print data
-    time.sleep(1)
-    dataset[...] = data
-    print dataset[0:4]
-
-    #group = f.create_group("somegroup")
-    #f['alias'] = h5py.SoftLink('/somegroup')
-    #print f['alias']
-
-    #dset = f.create_dataset("default", (100,))
-    #print dset
-
-    #arr = np.arange(100)
-    #print arr
-    #dset = f.create_dataset("init", data=arr)
-    #print dset
-    #
-    #dset = f.create_dataset("chunked", (100,100), chunks=(100, 100))
-    #print dset
-    #
-    #dset = f.create_dataset("autochunk", (10,10), chunks=True)
-    #print dset, dset.shape
-    #result = dset[1:6,[1,3,8]]
-    #print result.shape 
-
-    #print f.id
-    #print f.keys()
-    #grp = f.create_group("bar")
-    #print grp.name
-    #subgrp = grp.create_group("baz")
-    #print subgrp.name
-    #grp["name"] = 45
-    #out = grp["name"]
-    #print out
-    #myds = subgrp["MyDs"]
-    #print myds
-    #missing = subgrp["missing"]
-    #print missing
-    for i in xrange(2):
-        print i
-    f.close()
-#================================================================#
+    with open("./h5_output.dat",'w') as infile:
+        f = h5py.File('TMS1mmSingleCSAOut1_1.h5','r')           #open .h5 file in read-only mode
+        print f.keys()                                          #acquire dataset info 
+        data = f['C0'].value                                    #fetch 'C0' dataset value
+        print type(f['C0'].value)                                    #fetch 'C0' dataset value
+        print len(data[0])
+        for i in xrange(len(data[0])):
+            if i < 1000000:                                     #fetch 100w data from 'C0' dataset
+                infile.write("%.2f\n"%data[0][i])
+            else:
+                break
+        f.close()                                               #close hdf5 file                          
+#----------------------------------------------------------------#
 if __name__ == "__main__":
     sys.exit(main())
