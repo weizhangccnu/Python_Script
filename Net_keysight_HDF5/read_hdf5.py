48b48fda716fe61ca7bd9f1f0e840bdb4d5d94a7
weizhangccnu <296448435@qq.com>
2017-09-15 22:27:11 +0800

    Corretly return c hdf5 value

--- Net_keysight_HDF5/read_hdf5.py
+++ Net_keysight_HDF5/read_hdf5.py
@@ -7,13 +7,17 @@ from mpi4py import MPI
 import sys
 import os
 import time
-import h5py
+
 #------------------------------------------------------------------------#
 ##define a class
 #
 class StructPointer(Structure):
     pass
-
+#------------------------------------------------------------------------#
+##define a class
+#
+class HDF5IO_waveform_file(Structure):
+    pass
 #------------------------------------------------------------------------#
 ## compare .dat format file with .h5 format file
 #
@@ -32,12 +36,26 @@ def compare():
             outfile.write("%d\n"%i) 
 #------------------------------------------------------------------------#
 def main():
-    hdf5iodll = cdll.LoadLibrary("./hdf5io.so")
+    #hdf5iodll = cdll.LoadLibrary("./hdf5io.so")        #the first method to invoke dll library on Linux
+    hdf5iodll = CDLL("./hdf5io.so")                     #the second method to invoke dll library on linux 
     #compare() 
-    StructPointer._fields_ = [('x', c_int),('y',c_int),('next', POINTER(StructPointer))]
+    StructPointer._fields_ = [('x', c_long),('y',c_int),('next', POINTER(StructPointer))]
     hdf5iodll.test.restype = POINTER(StructPointer)
-    p = hdf5iodll.test()
+    p = hdf5iodll.test(49,2)
+    print p.contents.x
     print p.contents.y
+
+    HDF5IO_waveform_file._fields_ = [('waveFid', c_int), ('nPt', c_long), ('nCh', c_long), ('nWfmPerChunk', c_long), ('nEvent', c_long), ('next', POINTER(HDF5IO_waveform_file))]
+    hdf5iodll.HDF5IO_open_file.restype = POINTER(HDF5IO_waveform_file)
+    p = hdf5iodll.HDF5IO_open_file("TMS1mmSingleCSAOut1_1.h5", 100, 1)
+    print p.contents.waveFid
+    print p.contents.nPt
+    print p.contents.nCh
+    print p.contents.nWfmPerChunk
+    print p.contents.nEvent
+
+    q = create_string_buffer("Hello", 10)
+    print sizeof(q), repr(q.raw)
     print "Ok!"
 #------------------------------------------------------------------------#
 if __name__ == "__main__":
