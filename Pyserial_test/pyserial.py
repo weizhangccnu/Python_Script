74b5c25ce45ae8c366b155b85c17b9fb0d14739d
weizhangccnu <296448435@qq.com>
2017-12-22 09:01:28 +0800

    upload pyserial test script

---
+++ Pyserial_test/pyserial.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import os
+import sys
+import time
+import serial
+#-----------------------------------------------#
+def main():
+    print "OK!"
+#-----------------------------------------------#
+if __name__ == "__main__":
+    main()
