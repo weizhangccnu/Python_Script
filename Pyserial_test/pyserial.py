d8c95acf26d2854fa2a61a2176321f2ba4635a98
weizhangccnu <296448435@qq.com>
2017-12-22 14:48:13 +0800

    atuomatically discern operating system

--- Pyserial_test/pyserial.py
+++ Pyserial_test/pyserial.py
@@ -5,10 +5,16 @@ import sys
 import time
 import struct
 import serial
+import platform
 #=================================================================#
 ## main function
 def main():
-    ser = serial.Serial('/dev/ttyS0', 9600, timeout=0.5)    #open serial port
+    if platform.system() == "Linux":
+        print "The operating system is Linux!"
+        ser = serial.Serial('/dev/ttyS0', 9600, timeout=0.5)    #open serial port
+    else:
+        print "The operating system is Windows!"
+        ser = serial.Serial('COM1', 9600, timeout=0.5)    #open serial port
     for i in xrange(1000000):               #read 100 charater from stm32
         ser.write('a')                      #sent charater 'a'
         print int(struct.unpack('B',ser.read(1))[0])
