42042b29ad5438ecbda71fcb7565800c631e1d51
weizhangccnu <296448435@qq.com>
2017-12-22 10:02:16 +0800

    pyserial UART communicate with stm32 correctly

--- Pyserial_test/pyserial.py
+++ Pyserial_test/pyserial.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 import os
 import sys
 import time
+import struct
 import serial
 #-----------------------------------------------#
+## main function
 def main():
-    print "OK!"
+    ser = serial.Serial('/dev/ttyS0', 9600, timeout=0.5)    #open serial port
+    for i in xrange(1000000):               #read 100 charater from stm32
+        ser.write('a')                      #sent charater 'a'
+        print int(struct.unpack('B',ser.read(1))[0])
+        time.sleep(0.05)                #delay
+    print "OK!"                         #stop
 #-----------------------------------------------#
+## if statement
 if __name__ == "__main__":
-    main()
+    main()                              #execute main function
