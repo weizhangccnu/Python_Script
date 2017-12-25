6c5598fe43a81cabfffb5b8a4ecf13f0af50b434
weizhangccnu <296448435@qq.com>
2017-12-25 18:04:04 +0800

    Change voltage correctly

--- SMU_2450/KEITHLEY_2450.py
+++ SMU_2450/KEITHLEY_2450.py
@@ -7,17 +7,28 @@ import socket
 import platform
 import subprocess
 import numpy as np
-#hostname = '192.168.1.119'                  #wire network hostname
 hostname = '192.168.2.100'                  #wire network hostname
 port = 5025                                 #host tcp port number
 #-------------------------------------------------------------------#
 def main():
+    ss.send("*IDN?\n")                                          #command terminated with '\n'
+    print "Instrument ID: %s"%ss.recv(50)
+    ss.send("*RST\n")                                           #command terminated with '\n'
+    ss.send(":SOUR:FUNC VOLT\n")
+    ss.send(':SENS:FUNC "CURR"\n')
+    ss.send("SENS:CURR:RANG 1\n")
+    ss.send("DISP:DIG 5\n")
+    ss.send("OUTP ON\n")
+    for i in xrange(30):
+        ss.send(":SOUR:VOLT %d\n"%i)
+        time.sleep(1)
+        ss.send(":READ?\n")
+        print ss.recv(100)
+    ss.close()                                                  #close socket
     print "Ok!"
 #-------------------------------------------------------------------#
 if __name__ == "__main__":
     ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #init local socket handle
     ss.connect((hostname, port))                                #connect to the instrument 
     main()
-    ss.send("*IDN?\n")
-    print "Instrument ID: %s"%ss.recv(50)
-    ss.close()                                                  #close socket
+
