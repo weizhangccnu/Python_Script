eb4bbcca5fc6cebe2eb792db1814585023605fe8
weizhangccnu <296448435@qq.com>
2017-12-27 22:36:03 +0800

    CC mode and control correctly

--- Elect_Load_IT8811/Elect_Load.py
+++ Elect_Load_IT8811/Elect_Load.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import time                         #import moudle
 import os
 import sys
 import subprocess
 import math
 import visa                     #windows instrument driver
+'''
+PC communicate with IT8811 electronic load via USB on Windows operating system
+Electronic load serial number: 13000919
+'''
 #*******************************************************************#
 def main():
 	rm = visa.ResourceManager()
@@ -14,8 +17,14 @@ def main():
 	#instr = rm.open_resource('USB0::0x1AB1::0x09C4::DM3R172501112::INSTR')				#Rigol DM3058E
 	instr = rm.open_resource('USB0::0xFFFF::0x8800::017001106771101007::INSTR')			#IT8811
 	print instr.query("*IDN?")
-	print "OK!"
+	for i in xrange(32):
+		step = i * 0.2
+		instr.write("SOURce:CURRent %f"%step)
+		time.sleep(1)
+		print instr.query(":FETCh:VOLTage:DC?")                                             #Fetch the voltage value
+		print instr.query(":FETCh:Current:DC?")                                             #Fetch the voltage value
 	instr.close()
+	print "OK!"
 #*******************************************************************#
 if __name__ == '__main__':
 	main()
