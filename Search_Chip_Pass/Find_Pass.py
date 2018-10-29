2f3826dc16564f970b1dc188ace169c51536e168
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2018-10-29 17:32:19 -0500

    record NP NF chip location

--- Search_Chip_Pass/Find_Pass.py
+++ Search_Chip_Pass/Find_Pass.py
@@ -2,49 +2,41 @@ import sys
 import csv
 import json
 import os, os.path
-# import pandas as pd
-# from pandas import ExcelWriter
-# from pandas import ExcelFile
 from collections import OrderedDict
 from pyexcel_xlsx import get_data
 from pyexcel_xlsx import save_data
 #======================================================================#
+## read and save excel file
 def read_excel():
-    rd_data = get_data("LOCx2 Passed Chips in Trays.xlsx")
-    print rd_data.keys()                                                       #print keys
-    sv_data = OrderedDict()
-    print rd_data[rd_data.keys()[0]]
-    print rd_data[rd_data.keys()[1]]
-    print rd_data[rd_data.keys()[2]]
-    len1 = len(rd_data)
-    len2 = len(rd_data.values()[0])
-    len3 = len(rd_data.values()[0][0])
-    print len1, len2, len3
-    sheet_data = [[[0 for y in xrange(len3)] for x in xrange(len2*2)] for z in xrange(len1)]
-    print len(sheet_data)
-    print sheet_data
-    print sheet_data[0][0]
-
-    for i in xrange(len1):
-        for j in xrange(len2):
-            for k in xrange(len3):
-                print i, j, k
-                sheet_data[i][j*2][k] =  rd_data[rd_data.keys()[i]][j][k]
-                # print rd_data[rd_data.keys()[i]][j][k]
-                sheet_data[i][j*2+1][k] = search_file(rd_data[rd_data.keys()[i]][j][k])
-        sv_data.update({rd_data.keys()[i]: sheet_data[i]})
-    save_data("LOCx2 Passed Chips in Trays_Runpassed.xlsx", sv_data)
-    # print rd_data.values()[0][1]
-
+    with open("Record.dat", 'w') as recordfile:
+        rd_data = get_data("LOCx2 Passed Chips in Trays.xlsx")
+        print rd_data.keys()                                                       #print keys
+        sv_data = OrderedDict()
+        print rd_data[rd_data.keys()[0]]
+        print rd_data[rd_data.keys()[1]]
+        print rd_data[rd_data.keys()[2]]
+        len1 = len(rd_data)
+        len2 = len(rd_data.values()[0])
+        len3 = len(rd_data.values()[0][0])
+        print len1, len2, len3
+        sheet_data = [[[0 for y in xrange(len3)] for x in xrange(len2*2)] for z in xrange(len1)]
+        for i in xrange(len1):
+            for j in xrange(len2):
+                for k in xrange(len3):
+                    print i, j, k
+                    sheet_data[i][j*2][k] =  rd_data[rd_data.keys()[i]][j][k]
+                    sheet_data[i][j*2+1][k] = search_file(rd_data[rd_data.keys()[i]][j][k])
+                    if search_file(rd_data[rd_data.keys()[i]][j][k]) != "P":    # list NP, NF chip location
+                        recordfile.write("%s %2d %2d %s\n"%(rd_data.keys()[i], j, k, search_file(rd_data[rd_data.keys()[i]][j][k])))
+            sv_data.update({rd_data.keys()[i]: sheet_data[i]})                  # sheet_name and sheet_data
+        save_data("LOCx2 Passed Chips in Trays_Runpassed.xlsx", sv_data)        # save excel file
 #======================================================================#
+# main function
 def main():
-    excel_filename = ['LOCx2_13_14_15.xlsx']
-    read_excel()
-    # for i in xrange(3):
-    #     search_file(123)
-    print "Ok"
+    read_excel()                        # Execute main function
+    print "Ok"                          # execute ended
 #======================================================================#
-## search file
+## search file in directory
 def search_file(chip_id):
     Check_err = 0
     File_exist = 0
