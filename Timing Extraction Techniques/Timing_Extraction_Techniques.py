e252417d6d5ff74c65623e085cdca7ffc5d0337a
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2018-11-05 15:59:58 -0600

    Timing extraction thchniques

---
+++ Timing Extraction Techniques/Timing_Extraction_Techniques.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import time
+'''
+Timing extraction techniques simulation: single-threshold, multi-threshold, constant fraction discriminator, waveform sampling
+@author: Wei Zhang
+@date: Nov 5, 2018
+@address: SMU
+'''
