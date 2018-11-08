d2fe318445f4794e66ae1b133c368ef27a704b2f
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2018-11-08 14:04:57 -0600

    ETL project simulation

--- Timing Extraction Techniques/Timing_Extraction_Techniques.py
+++ Timing Extraction Techniques/Timing_Extraction_Techniques.py
@@ -1,14 +1,11 @@
 import os
 import sys
 import time
-import math
 
-import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
-
-from sympy.integrals import laplace_transform
-from sympy.abc import t, s
+from sympy.abc import a, t, x, s
+from sympy.integrals.transforms import laplace_transform
 '''
 Timing extraction techniques simulation: single-threshold, multi-threshold, constant fraction discriminator, waveform sampling
 @author: Wei Zhang
@@ -17,49 +14,54 @@ Timing extraction techniques simulation: single-threshold, multi-threshold, cons
 '''
 #======================================================================#
 ## plot parameters
-# hist_bins = 30                  # histogram bin counts
+hist_bins = 40                  # histogram bin counts
 lw_grid = 0.5                   # grid linewidth
 fig_dpi = 800                   # save figure's resolution
-#==========================================================================================================#
-## read signal file
+
+filepath = "55micron_G_15_Landau\wf_BV300_DV20_W1_H55_SP500_SW490_G15_T250_A0_D0_BW0_C3_L5_GS0.1_BBBW0.7_MIPL0_3.txt"
+#===========================================================================================================#
+## read_signal_file
 def read_signal_file():
-    with open("55micron_G_15_Landau/wf_BV300_DV20_W1_H55_SP500_SW490_G15_T250_A0_D0_BW0_C3_L5_GS0.1_BBBW0.7_MIPL0_1.txt", 'r') as signal_file:
-        # i = 0
+    with open(filepath, 'r') as signal_file:
         Data = [[], []]
         for line in signal_file.readlines():
-            # i += 1
-            # if i >= 2:                                          # read file from third row
-                # print i, line
-            Data[0] += [float(line.split()[0]) * 1e9]       # change y-axis to ns
-            Data[1] += [float(line.split()[1]) * 1e6]       # change x-axis to uV
+            Data[0] += [float(line.split()[0]) * 1e9]
+            Data[1] += [float(line.split()[1]) * 1e6]
         return Data
-#==========================================================================================================#
-## plot signal file
+#===========================================================================================================#
+## plot_signal_file
+#@param[in] x: x-axis data
+#@param[in] y: y-axis data
 def plot_signal_file(x, y):
-    print x
-    print y
-    plt.plot(x, y, color='b', linewidth=1.5, label='Qin vs TOA')
-    # plt.ylim(-30, 5)
-    plt.title("Input signal", family="Times New Roman", fontsize=12)
+    plt.plot(x, y, 'r-', linewidth=0.5, label='Sensor output signal')
+    plt.title("Sensor Output Signal", family="Times New Roman", fontsize=12)
     plt.xlabel("Time [ns]", family="Times New Roman", fontsize=10)
-    plt.ylabel("Amplitude [uC]", family="Times New Roman", fontsize=10)
+    plt.ylabel("Current [uA]", family="Times New Roman", fontsize=10)
 
     plt.xticks(family="Times New Roman", fontsize=8)
     plt.yticks(family="Times New Roman", fontsize=8)
-    plt.grid(linestyle='-.', linewidth=lw_grid)
     plt.legend(fontsize=8, edgecolor='green')
-    plt.savefig("Input_Signal.png", dpi=fig_dpi)
+    plt.grid(linestyle='-.', linewidth=lw_grid)
+    plt.savefig("Sensor_output_signal.png", dpi=fig_dpi)
     # plt.show()
     plt.clf()
-#==========================================================================================================#
+#===========================================================================================================#
+## using Laplace transfer to calculate Vout(s) of Pre-Amplifier
+##   Vout(s)                                          RL*(RF * gm -1)
+##   -------  = H(s) = -------------------------------------------------------------------------------
+##    Iin(S)            S**2 * RF * RL * Cin * CL + S * (RF * Cin + RL * CL + RL * Cin) + 1 - gm * RL
+##  first step: calculate h(t) via laplace_inversetransfer
+##  second step: calculate convolution between h(t) and  Iin(t) : Vout(t) = Iin(t) (x) h(t)
+def preamp_output():
+    pass
+#===========================================================================================================#
 ## main function
 def main():
-    Data = read_signal_file()
-    plot_signal_file(Data[0], Data[1])
-    print laplace_transform(t**2, t, s)
+    data = read_signal_file()
+    print data
+    plot_signal_file(data[0], data[1])
     print "Ok!"
-
-#==========================================================================================================#
+#===========================================================================================================#
 ## if statement
 if __name__ == '__main__':
     main()
