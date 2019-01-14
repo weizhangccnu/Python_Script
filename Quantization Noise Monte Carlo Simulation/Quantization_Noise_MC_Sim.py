487a06b512778e6dcb6813b20d422e29572c656a
weizhangccnu <wzhang@mails.ccnu.edu.cn>
2019-01-14 15:19:16 -0600

    add sample generate

---
+++ Quantization Noise Monte Carlo Simulation/Quantization_Noise_MC_Sim.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import time
+'''
+TDC Quantization Noise Monte Carlo simulation via Python Script.
+@author: Wei Zhang
+@date: Jan 14, 2019
+@address: SMU Dallas, TX
+'''
+#==================================================================================#
+## sample generator to generate 10000 Guassian data with mu = 0 ps, sigma = 30 ps
+def sample_generate():
+    pass
+
+#==================================================================================#
+## main function
+def main():
+    print "pass"
+#==================================================================================#
+## if statement
+if __name__ == '__main__':
+    main()
