eb1ccbc74bc5f3dad25b457e1f9c5f8b65174f66
Zhang <weizhang@smu.edu>
2019-06-24 17:22:38 -0500

    add comments

--- 63 Delay Cells Measure/TDC_Linearity_Measurement.py
+++ 63 Delay Cells Measure/TDC_Linearity_Measurement.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
 from mpl_toolkits.axes_grid1.inset_locator import mark_inset
 '''
-This script is used to measure the linearity of 63 Delay Cells TDC
+This script is used to encode the TOA, TOT, and Calibration transfer function of 63 Delay Cells TDC
 @author: Wei Zhang
 @date: Jan 31, 2019
 @address: SMU Dallas, TX
@@ -17,6 +17,7 @@ This script is used to measure the linearity of 63 Delay Cells TDC
 lw_grid = 0.5                   # grid linewidth
 fig_dpi = 800                   # save figure's resolution
 #================================================================================================#
+## read_csvfile: to read .csv origin simulation data.
 def read_csvfile():
     with open("Delay_Cells_63_TOA_TOT_Tap31_1ps.csv", 'rb') as csvfile:
         reader = csv.reader(csvfile)
@@ -29,18 +30,20 @@ def read_csvfile():
                     Data[i-2] += [float(row[col*2+1]) > 0.6 and 1 or 0]
     return Data
 #================================================================================================#
+## TOARaw_Transfer_Function: Encode TOA Raw data to 10-bit binary code.
+#@param[in]: Data: TOA, TOT, and Calibration simulation data.
 def TOARaw_Transfer_Function(Data):
     TOA_codeReg = []
-    for i in xrange(len(Data)):
+    for i in xrange(len(Data)):                                     # fetch TOA Raw data
         if Data[i][3:66].count(1) > 1:
             print Data[i][3:66].index(1), "Wrong"
         else:
             TOARaw = Data[i][3:66].index(1)
-        TOACntA = Data[i][68]<<2 | Data[i][67]<<1 | Data[i][66]
-        TOACntB = Data[i][71]<<2 | Data[i][70]<<1 | Data[i][69]
-        if Data[i][0] == 1:
+        TOACntA = Data[i][68]<<2 | Data[i][67]<<1 | Data[i][66]     # TOA Counter A number
+        TOACntB = Data[i][71]<<2 | Data[i][70]<<1 | Data[i][69]     # TOT Counter B number
+        if Data[i][0] == 1:                                         # TOARawdata[62] = 1; How to choose CounterA and CounterB
             TOA_Code = ((TOACntB)<<1 | 0) * 63 + TOARaw + 1
-        else:
+        else:                                                       # TOARawdata[62] = 0;
             TOA_Code = (((TOACntA-1)<<1) | 1) * 63 + TOARaw + 1
         # print
         # print Data[i][0:1], TOARaw, TOACntA, TOACntB, TOA_Code
@@ -51,7 +54,7 @@ def TOARaw_Transfer_Function(Data):
         x += [float(i)]
     print len(x)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots()                                        # Plot TOA transfer function
     ax.plot(x, TOA_codeReg, color='r',marker='X', linewidth=0.2, markersize=0.02, label='TOA Transfer Function')
     plt.title("63 Delay Cells TDC TOA Transfer Function Step = 1ps nominal", family="Times New Roman", fontsize=12)
     plt.xlabel("TOA_CK [ns]", family="Times New Roman", fontsize=10)
@@ -68,22 +71,24 @@ def TOARaw_Transfer_Function(Data):
     plt.yticks(family="Times New Roman", fontsize=7)
     plt.grid(linestyle='-.', linewidth=lw_grid)
 
-    plt.savefig("TOA_Transfer_Function_1ps_nominal.png", dpi=fig_dpi, bbox_inches='tight')         # save figure
+    plt.savefig("TOA_Transfer_Function_1ps_nominal.pdf", dpi=fig_dpi, bbox_inches='tight')         # save figure
     plt.clf()
 #================================================================================================#
+## TOTRaw_Transfer_Function: Encode TOT Raw data to 9-bit binary code. 
+#@param[in]: Data: TOA, TOT, and Calibration simulation data.
 def TOTRaw_Transfer_Function(Data):
     TOT_codeReg = []
     for i in xrange(len(Data)):
-        if Data[i][72:104].count(1) > 1:
+        if Data[i][72:104].count(1) > 1:                            # Get TOT Raw data
             print Data[i][72:104].index(1), "Wrong"
         else:
             TOTRaw = Data[i][72:104].index(1)
-        TOTCntA = Data[i][106]<<2 | Data[i][105]<<1 | Data[i][104]
-        TOTCntB = Data[i][109]<<2 | Data[i][108]<<1 | Data[i][107]
-        if TOTRaw == 31:
-            if Data[i][1] == 0:
+        TOTCntA = Data[i][106]<<2 | Data[i][105]<<1 | Data[i][104]  # TOT Counter A number
+        TOTCntB = Data[i][109]<<2 | Data[i][108]<<1 | Data[i][107]  # TOT Counter B number
+        if TOTRaw == 31:                                            # TOTRawdata = 31
+            if Data[i][1] == 0:                                     # TOTRaw[31] = 0
                 TOT_Code = ((TOTCntB)<<1 | 0) * 32 + TOTRaw + 1
-            else:
+            else:                                                   # TOTRaw[31] = 1
                 TOT_Code = ((TOTCntA-1)<<1 | 1) * 32 + TOTRaw + 1
         else:
             if Data[i][1] == 1:
@@ -98,7 +103,7 @@ def TOTRaw_Transfer_Function(Data):
         x += [float(i)]
     print len(x)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots()                                        # Plot TOT transfer function
     ax.plot(x, TOT_codeReg, color='r',marker='X', linewidth=0.2, markersize=0.02, label='TOT Transfer Function')
     plt.title("63 Delay Cells TDC TOT Transfer Function Step = 1ps nominal", family="Times New Roman", fontsize=12)
     plt.xlabel("TOT_CK [ns]", family="Times New Roman", fontsize=10)
@@ -115,10 +120,12 @@ def TOTRaw_Transfer_Function(Data):
     plt.yticks(family="Times New Roman", fontsize=7)
     plt.grid(linestyle='-.', linewidth=lw_grid)
 
-    plt.savefig("TOT_Transfer_Function_1ps_nominal.png", dpi=fig_dpi, bbox_inches='tight')         # save figure
+    plt.savefig("TOT_Transfer_Function_1ps_nominal.pdf", dpi=fig_dpi, bbox_inches='tight')         # save figure
     plt.clf()
 
 #================================================================================================#
+## CalRaw_Transfer_Function: Encode Calibration Raw data to 10-bit binary code.
+#@param[in]: Data: TOA, TOT, and Calibration simulation data.
 def CalRaw_Transfer_Function(Data):
     Cal_codeReg = []
     for i in xrange(len(Data)):
