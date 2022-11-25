a7f8b8d4e7561048223ae3da82a5bc0bf737b371
weizhangccnu <296448435@qq.com>
2022-11-25 09:44:21 +0800

    add Ev Tcp uv measurement

--- CL200A_Control/CL200A_Control.py
+++ CL200A_Control/CL200A_Control.py
@@ -19,18 +19,21 @@ def main():
 
     command_EvXY    = "0x02 0x30 0x30 0x30 0x32 0x31 0x33 0x30 0x30 0x03 0x30 0x33 0x0d 0x0a"
 
+    command_EvTcp   = "0x02 0x30 0x30 0x30 0x38 0x31 0x33 0x30 0x30 0x03 0x30 0x39 0x0d 0x0a"
+
     str_command_link     = ''.join([chr(int(i, 16)) for i in command_link.split()])
     str_command_hold     = ''.join([chr(int(i, 16)) for i in command_hold.split()])
     str_command_setEXT   = ''.join([chr(int(i, 16)) for i in command_setEXT.split()])
     str_command_measure  = ''.join([chr(int(i, 16)) for i in command_measure.split()])
     str_command_EvXY     = ''.join([chr(int(i, 16)) for i in command_EvXY.split()])
+    str_command_EvTcp     = ''.join([chr(int(i, 16)) for i in command_EvTcp.split()])
 
     ser = serial.Serial('COM9', baudrate=9600, bytesize=serial.SEVENBITS, parity=serial.PARITY_EVEN, stopbits=serial.STOPBITS_ONE)
     if ser.isOpen():
         print("serial port open success!")
     print("Serial port: %s"%ser.name)
 
-    ser.write(str_command_link.encode())                    # link CLA200A device
+    ser.write(str_command_link.encode())                      # link CLA200A device
     time.sleep(1)
     print("%s"%ser.readline())
     
@@ -45,22 +48,39 @@ def main():
     time.sleep(1)
     ser.write(str_command_measure.encode())                 # measure command 
 
+    # time.sleep(1)
+    # ser.write(str_command_EvXY.encode())                    # measure EvXY value
+    # time.sleep(1)
+
+    # EvXY_str = ser.readline()                               # read back EvXY value
+
+    # print(EvXY_str)
+    # print(EvXY_str.decode().split('+')[1], EvXY_str.decode().split('+')[2], EvXY_str.decode().split('+')[3][:-4])
+
+    # EV_value = int(EvXY_str.decode().split('+')[1][:-1]) * exp[int(EvXY_str.decode().split('+')[1][-1])]
+    # X_value = int(EvXY_str.decode().split('+')[2][:-1]) * exp[int(EvXY_str.decode().split('+')[2][-1])]
+    # Y_value = int(EvXY_str.decode().split('+')[3][:-4][:-2]) * exp[int(EvXY_str.decode().split('+')[3][:-4][-2])]
+
+    # print("EV value: %f"%EV_value)
+    # print("X value: %f"%X_value)
+    # print("Y value: %f"%Y_value)
+
+
     time.sleep(1)
-    ser.write(str_command_EvXY.encode())                    # measure EvXY value
+    ser.write(str_command_EvTcp.encode())                    # measure EvXY value
     time.sleep(1)
 
-    EvXY_str = ser.readline()                               # read back EvXY value
+    EvTcp_str = ser.readline()                               # read back EvXY value
 
-    print(EvXY_str)
-    print(EvXY_str.decode().split('+')[1], EvXY_str.decode().split('+')[2], EvXY_str.decode().split('+')[3][:-4])
+    print(EvTcp_str)
 
-    EV_value = int(EvXY_str.decode().split('+')[1][:-1]) * exp[int(EvXY_str.decode().split('+')[1][-1])]
-    X_value = int(EvXY_str.decode().split('+')[2][:-1]) * exp[int(EvXY_str.decode().split('+')[2][-1])]
-    Y_value = int(EvXY_str.decode().split('+')[3][:-4][:-2]) * exp[int(EvXY_str.decode().split('+')[3][:-4][-2])]
+    EV_value = int(EvTcp_str.decode().split('+')[1][:-1]) * exp[int(EvTcp_str.decode().split('+')[1][-1])]
+    Tcp_value = int(EvTcp_str.decode().split('+')[2][:-1]) * exp[int(EvTcp_str.decode().split('+')[2][-1])]
+    Delta_uv_value = int(EvTcp_str.decode().split('+')[3][:-4][:-2]) * exp[int(EvTcp_str.decode().split('+')[3][:-4][-2])]
 
     print("EV value: %f"%EV_value)
-    print("X value: %f"%X_value)
-    print("Y value: %f"%Y_value)
+    print("Tcp value: %f"%Tcp_value)
+    print("Delta uv value: %f"%Delta_uv_value)
 
     print("Ok!")
 
